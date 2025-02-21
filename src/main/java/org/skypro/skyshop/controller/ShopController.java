fb04d8c02c5603bc60b99a3f8820778b1bdaed72
1 <vasilijnenastev@gmail.com>
2025-02-21 13:05:54 +0400

    1/1

---
+++ src/main/java/org/skypro/skyshop/controller/ShopController.java
@@ -0,0 +1,39 @@
+package org.skypro.skyshop.controller;
+
+import org.skypro.skyshop.model.article.Article;
+import org.skypro.skyshop.model.product.Product;
+import org.skypro.skyshop.model.search.SearchResult;
+import org.skypro.skyshop.model.service.SearchService;
+import org.skypro.skyshop.model.service.StorageService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+@RestController
+public class ShopController {
+    private final StorageService storageService;
+    private final SearchService searchService;
+
+
+    public ShopController(StorageService storageService, SearchService searchService) {
+        this.storageService = storageService;
+        this.searchService = searchService;
+    }
+
+    @GetMapping("/products")
+    public Collection<Product> getAllProducts() {
+        return storageService.getProductMap().values();
+    }
+
+    @GetMapping("/articles")
+    public Collection<Article> getAllArticles() {
+        return storageService.getArticleMap().values();
+    }
+
+    @GetMapping("/search")
+    public Collection<SearchResult> search(@RequestParam("pattern") String pattern) {
+        return searchService.search(pattern).values();
+    }
+
+}
